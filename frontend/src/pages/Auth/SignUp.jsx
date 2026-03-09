881d02e60b8300e085ea5cede62f151ba386cfd6
Digvijay Narayan Pandey <digvijaynarayanpandey7@gmail.com>
2026-03-09 11:33:21 +0530

    fixed the signup page issue for mobile

--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -99,7 +99,7 @@ const SignUp = () => {
               placeholder="john@example.com"
               type="text"
             />
-            <div className="col-span-2">
+            <div className="md:col-span-2">
               <Input
                 value={password}
                 onChange={({ target }) => setPassword(target.value)}
